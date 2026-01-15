29773c7addbc76ed9a6cfd514f298730ffb3db8b
Vanxi <isacconsult@gmail.com>
2026-01-16 00:04:31 +0200

    modifying pack json

--- src/component/Oferta.js
+++ src/component/Oferta.js
@@ -9,7 +9,7 @@ class Oferta extends Component{
                 <div className="container">
                   <div className="section_title text-center mb_60">
                     <Reveal  effect="fadeInUp"><h2 className="mb_0 title_h2 t_color">OFERTA (SIMPLU, MONETIZABIL)</h2></Reveal>
-                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">ISAC Rapid</p></Reveal>
+                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">ISAC Rapid </p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">10 minute | 100 RON</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Instrucțiuni clare, aplicabile imediat.</p></Reveal>
 
