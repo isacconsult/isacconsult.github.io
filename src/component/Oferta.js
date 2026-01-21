92efa438b053f48f7ca745cf03b534a7ec068eaf
Vanxi <isacconsult@gmail.com>
2026-01-21 14:23:16 +0200

    css changes

--- src/component/Oferta.js
+++ src/component/Oferta.js
@@ -9,11 +9,13 @@ class Oferta extends Component{
                 <div className="container">
                   <div className="section_title text-center mb_60">
                     <Reveal  effect="fadeInUp"><h2 className="mb_0 title_h2 t_color">OFERTA (SIMPLU, MONETIZABIL)</h2></Reveal>
-                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">ISAC Rapid </p></Reveal>
+                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p" 
+                      style={{ fontSize: "18px", fontWeight: 'bold' }}>ISAC Rapid </p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">10 minute | 100 RON</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Instrucțiuni clare, aplicabile imediat.</p></Reveal>
-                    <p className="mb_0 title_p">|</p>
-                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">ISAC Strategy</p></Reveal>
+                    <p className="mb_0 title_p">✻ ✻ ✻</p>
+                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p"
+                      style={{ fontSize: "18px", fontWeight: 'bold' }}>ISAC Strategy</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">50 minute | 500 RON</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Claritate + reguli reutilizabile.</p></Reveal>
                   </div>
--- src/component/SchemaIsac.js
+++ src/component/SchemaIsac.js
@@ -7,16 +7,16 @@ class SchemaIsac extends Component{
         return(
             <section className={`${wClass}`} id="service">
                 <div className="container">
-                  <div className="section_title text-center mb_60">
+                  <div className="section_title text-center">
                     <Reveal  effect="fadeInUp"><h2 className="mb_0 title_h2 t_color">SCHEMA ISAC (LOGICĂ VIZUALĂ)</h2></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Tipologia lui</p></Reveal>
-                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">↓</p></Reveal>
+                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p" style={{ color: "#797979" }}>⬇️</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Comportament</p></Reveal>
-                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">↓</p></Reveal>
+                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">⬇️</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Context</p></Reveal>
-                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">↓</p></Reveal>
+                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">⬇️</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Acțiunea ta</p></Reveal>
-                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">↓</p></Reveal>
+                    <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">⬇️</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Rezultat</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">*</p></Reveal>
                     <Reveal  effect="fadeInUp" duration={1500}><p className="mb_0 title_p">Regulă:</p></Reveal>
